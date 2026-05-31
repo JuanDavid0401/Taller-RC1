a23ca0f96e6262c72bd477f4f2585d13a78f0c8d
Juan David <juan.cartagena.geate@udla.edu.ec>
2026-05-31 18:50:19 -0500

    Resultados por estudiante

--- TallerRC1.c
+++ TallerRC1.c
@@ -30,3 +30,22 @@ int main() {
             } while(notas[i][j] < 0 || notas[i][j] > 10);
         }
     }
+
+    printf(" \n RESULTADOS POR ESTUDIANTE \n");
+    
+    for(int i = 0; i < n; i++) {
+        float suma_est = 0;
+        
+        float max_est = notas[i][0]; 
+        float min_est = notas[i][0];
+
+        
+        for(int j = 0; j < 3; j++) {
+            suma_est += notas[i][j];
+            if(notas[i][j] > max_est) max_est = notas[i][j];
+            if(notas[i][j] < min_est) min_est = notas[i][j];
+        }
+        
+        printf("Estudiante %d -> Promedio: %.2f | Max: %.2f | Min: %.2f\n", 
+               i + 1, suma_est / 3.0, max_est, min_est);
+    }
