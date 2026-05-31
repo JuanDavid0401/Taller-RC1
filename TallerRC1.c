f968ed03f905b07cc3b3019162973a121062979d
Juan David <juan.cartagena.geate@udla.edu.ec>
2026-05-31 18:32:56 -0500

    Implementacion de ingreso de notas por asignatura y corroboracion de notas en el rango

--- TallerRC1.c
+++ TallerRC1.c
@@ -12,3 +12,21 @@ int main() {
         printf("Error: Debe haber al menos 1 estudiante.\n");
         return 1;
     }
+
+    float notas[n][3];
+
+    
+    printf("\n INGRESO DE NOTAS \n");
+    for(int i = 0; i < n; i++) {
+        printf("\n Estudiante %d \n", i + 1);
+        for(int j = 0; j < 3; j++) {
+            do {
+                printf("Calificacion para Asignatura %d (0 a 10): ", j + 1);
+                scanf("%f", &notas[i][j]);
+                
+                if(notas[i][j] < 0 || notas[i][j] > 10) {
+                    printf("-> Nota invalida. Intente de nuevo.\n");
+                }
+            } while(notas[i][j] < 0 || notas[i][j] > 10);
+        }
+    }
